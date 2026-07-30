e23a89c53e6d6210bc684a6e494ffb9345c379cc
Matthias Röder <mr1977@gmail.com>
2026-07-30 17:25:27 +0200

    Upgrade site to Astro 7

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://themindshift.global',
+  compressHTML: true,
   integrations: [
     sitemap({
       filter: (page) => page !== 'https://themindshift.global/writings/'
